8303b0f715f7fad23a46beb0cf8caa6cf264f1fd
abdansm <abdan.mardha@gmail.com>
2025-06-10 22:14:41 +0700

    delete echo

--- check_login.php
+++ check_login.php
@@ -4,19 +4,12 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 
 
-function debug_to_console($data) {
-    $output = $data;
-    if (is_array($output))
-        $output = implode(',', $output);
 
-    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
-}
 
 
 if($username == 'penjaga' && $password == 'penjaga_123'){
     $_SESSION["username"] = "penjaga";
-    debug_to_console("sesuatu terjadi");
-    debug_to_console($_SESSION["username"]);
+    
     
     header("location:session.php?username=$username");
 }
